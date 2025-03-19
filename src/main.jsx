5ebd4559aad11973c2b3b9c8330eb220be4f1c03
Luan Marcelino de Souza <luan.souza@grupoprovac.com.br>
2025-03-19 09:44:03 -0300

    conceito de State

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+// renderiza nossa aplicação react dentro da div root que está no index.html
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
